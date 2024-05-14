93fef7d841ab7d449e6d1ab82051502adf5559b0
danoctavian <danoctavian91@gmail.com>
2024-05-15 01:21:48 +0300

    Mainnet Release - v0.0.3

---
+++ src/interfaces/IynETH.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BSD 3-Clause License
+pragma solidity ^0.8.24;
+
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC20Permit} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol";
+
+interface IynETH is IERC20 {
+    function withdrawETH(uint256 ethAmount) external;
+    function processWithdrawnETH() external payable;
+    function receiveRewards() external payable;
+    function pauseDeposits() external;
+    function unpauseDeposits() external;
+
+    
+    /// @notice Allows depositing ETH into the contract in exchange for shares.
+    /// @param receiver The address to receive the minted shares.
+    /// @return shares The amount of shares minted for the deposited ETH.
+    function depositETH(address receiver) external payable returns (uint256 shares);
+}
